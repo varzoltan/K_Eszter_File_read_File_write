57bc3024dc12acebdb44237b0fb61a733591a3a7
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-02-08 15:14:33 +0100

    Add project files.

---
+++ K_Eszter_File_read_File_write/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_Eszter_File_read_File_write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
