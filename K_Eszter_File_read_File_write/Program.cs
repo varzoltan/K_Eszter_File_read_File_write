e35b3ab04b3a9ce1bbd7cfd186d61acccfc3ecf8
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-02-08 17:25:14 +0100

    15:15

--- K_Eszter_File_read_File_write/Program.cs
+++ K_Eszter_File_read_File_write/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Névtér változás: InputOutput
+using System.IO;
 
 namespace K_Eszter_File_read_File_write
 {
@@ -10,7 +12,33 @@ namespace K_Eszter_File_read_File_write
     {
         static void Main(string[] args)
         {
+            //fájlból olvasás
+            string[] fajlbol_olvas = new string[500];
+            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\eszter.txt");
+            int i = 0;
+            while (!olvas.EndOfStream)
+            {
+                fajlbol_olvas[i] = olvas.ReadLine();
+                i++;
+            }
+            olvas.Close();
 
+            //Írjuk ki a tömb elemit
+            //Console.WriteLine(fajlbol_olvas[0]);
+            for (int j = 0;j<i;j++)
+            {
+                Console.WriteLine(fajlbol_olvas[j]);
+            }
+
+
+            //fájlba írás
+            StreamWriter ir = new StreamWriter(@"C:\Users\Rendszergazda\eszter_ir.txt");
+            for (int j = 0;j<i;j++)
+            {
+                ir.Write(fajlbol_olvas[j] + " ");
+            }
+            ir.Close();
+            Console.ReadKey();
         }
     }
 }
